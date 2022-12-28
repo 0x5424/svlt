69cacf8aaa783b30931cd75950d8de7869e6d915
Trevor <trevor@osrs.tools>
2022-12-28 22:13:01 +0900

    Add svelte typescript support
    * Add `svelte-preprocess`
    
    * Re-use vite dependency, `esbuild`, to transpile content within svelte <script> tags

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import sveltePreprocess from 'svelte-preprocess'
+import { transformSync } from 'esbuild'
+
+// Use built-in ts loader from esbuild (no direct TS dependency!)
+const svelteConfig = {
+  preprocess: sveltePreprocess({
+    typescript: ({ content }) => transformSync(content, { loader: 'ts' })
+  })
+}
 
 export default defineConfig({
-  plugins: svelte()
+  plugins: svelte(svelteConfig)
 })
